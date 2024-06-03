76d1e4e85186639fa1ad337bf8c0c28d47632873
Sriharsha Ponakala <63137464+sri1437@users.noreply.github.com>
2024-06-03 02:27:49 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -2,3 +2,13 @@
 function downloadResume() {
     window.location.href = 'Sriharsha Resume.pdf';
 }
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
